d0a92d9f04750c45e6e133431724b17718ee87cf
Alexander Prokhorov <alexprokhorov@yandex-team.ru>
2022-07-16 18:19:59 +0300

    Increment 14. Split for goroutines (1)

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"aprokhorov-praktikum/cmd/agent/config"
@@ -58,44 +60,105 @@ func main() {
 	tickerPoll := time.NewTicker(pollInterval)
 	tickerSend := time.NewTicker(sendInterval)
 
-	for {
-		select {
-		case <-tickerPoll.C:
-			err := NewMetrics.PollMemStats(conf.MemStatMetrics)
-			errHandle("Poller error: %s", err, logger)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 
-			err = NewMetrics.PollRandomMetric()
-			errHandle("Poller error: %s", err, logger)
+	go func(ctx context.Context, signal <-chan time.Time, wgr *sync.WaitGroup, metrics *poller.Poller, metricList []string, log *zap.Logger) {
+		for {
+			select {
+			case <-signal:
+				err := metrics.PollMemStats(metricList)
+				if err != nil {
+					log.Error("Poller error: " + err.Error())
+				}
+				err = metrics.PollRandomMetric()
+				if err != nil {
+					log.Error("Poller error: " + err.Error())
+				}
+				counter, err := metrics.Storage.Read("counter", "PollCount")
+				if err != nil {
+					log.Error("Poller error: " + err.Error())
+				}
+				log.Info(fmt.Sprintf("Poll Count: %v", counter))
+			case <-ctx.Done():
+				wgr.Done()
+				return
+			}
+		}
+	}(context.Background(), tickerPoll.C, wg, NewMetrics, conf.MemStatMetrics, logger)
 
-			counter, err := NewMetrics.Storage.Read("counter", "PollCount")
-			errHandle("Poller error: %s", err, logger)
+	wg.Add(1)
+	go func(ctx context.Context, signal <-chan time.Time, s *sender.Sender, metrics *poller.Poller, batchStatus bool, key string, log *zap.Logger) {
+		for {
+			select {
+			case <-signal:
+				log.Info("Send Data to Server")
 
-			logger.Info(fmt.Sprintf("Poll Count: %v", counter))
+				metricsData, err := metrics.Storage.ReadAll()
+				if err != nil {
+					log.Error("Can't read mertics from storage: " + err.Error())
+				}
 
-		case <-tickerSend.C:
-			logger.Info("Send Data to Server")
-
-			metrics, err := NewMetrics.Storage.ReadAll()
-			errHandle("can't read metrics from storage: %s", err, logger)
-
-			// Обновляем либо батчем, либо по одному
-			switch conf.Batch {
-			case true:
-				go func(metric map[string]map[string]string, key string) {
-					err = send.SendMetricBatch(metric, key)
-					errHandle("Sender Batch error: %s", err, logger)
-				}(metrics, conf.Key)
-			case false:
-				for metricType, values := range metrics {
-					for metricName, metricValue := range values {
-						go func(mtype string, name string, value string) {
-							err := send.SendMetric(mtype, name, value, conf.Key)
-							errHandle("Sender error: %s", err, logger)
-						}(metricType, metricName, metricValue)
+				// Обновляем либо батчем, либо по одному
+				switch batchStatus {
+				case true:
+					go func() {
+						err = s.SendMetricBatch(metricsData, key)
+						if err != nil {
+							log.Error("Sender Batch: " + err.Error())
+						}
+					}()
+				case false:
+					for metricType, values := range metricsData {
+						for metricName, metricValue := range values {
+							go func(mtype string, name string, value string) {
+								err := send.SendMetric(mtype, name, value, key)
+								if err != nil {
+									log.Error("Sender Simple: " + err.Error())
+								}
+							}(metricType, metricName, metricValue)
+						}
 					}
 				}
+			case <-ctx.Done():
+				wg.Done()
+				return
 			}
-
 		}
-	}
+
+	}(context.Background(), tickerSend.C, send, NewMetrics, conf.Batch, conf.Key, logger)
+
+	/*
+		for {
+			select {
+			case <-tickerPoll.C:
+
+			case <-tickerSend.C:
+				logger.Info("Send Data to Server")
+
+				metrics, err := NewMetrics.Storage.ReadAll()
+				errHandle("can't read metrics from storage: %s", err, logger)
+
+				// Обновляем либо батчем, либо по одному
+				switch conf.Batch {
+				case true:
+					go func(metric map[string]map[string]string, key string) {
+						err = send.SendMetricBatch(metric, key)
+						errHandle("Sender Batch error: %s", err, logger)
+					}(metrics, conf.Key)
+				case false:
+					for metricType, values := range metrics {
+						for metricName, metricValue := range values {
+							go func(mtype string, name string, value string) {
+								err := send.SendMetric(mtype, name, value, conf.Key)
+								errHandle("Sender error: %s", err, logger)
+							}(metricType, metricName, metricValue)
+						}
+					}
+				}
+
+			}
+		}
+	*/
+	wg.Wait()
 }
